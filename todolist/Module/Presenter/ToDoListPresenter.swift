96619cc57601b960d7438c0b3b5755704a2d9cb3
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-11-25 18:02:25 +0400

    [CleanCode]

--- todolist/Module/Presenter/ToDoListPresenter.swift
+++ todolist/Module/Presenter/ToDoListPresenter.swift
@@ -8,103 +8,6 @@
 import Foundation
 import CoreData
 
-//OLD
-/*
- enum FilterOption {
-     case all
-     case completed
-     case active
- }
-
- final class ToDoListPresenter: ObservableObject {
-     @Published var todos: [ToDo] = []
-     private var filterOption: FilterOption = .all // По умолчанию показываем все задачи
-     private let interactor: ToDoListInteractor
-     private let router: ToDoListRouter
-     private var searchText: String = "" // Текст поиска
-     
-     init(interactor: ToDoListInteractor, router: ToDoListRouter) {
-         self.interactor = interactor
-         self.router = router
-         Task {
-             await loadDataFromAPI()
-         }
-         fetchToDos()
-
-     }
-     
-     // Обновленный метод для получения задач с учетом текста поиска
-     func fetchToDos() {
-         // Получаем задачи с учетом фильтров и поиска
-         let fetchedTodos = interactor.fetchToDos(filter: filterOption, searchText: searchText)
-         // Обновляем @Published свойство для перерисовки интерфейса
-         DispatchQueue.main.async {
-             self.todos = fetchedTodos // Убедитесь, что создается новый массив
-         }
-     }
-     
-     // Новый метод для фильтрации по поисковому тексту
-     func filterToDos(by searchText: String) {
-         self.searchText = searchText
-         fetchToDos()
-     }
-     
-     func loadDataFromAPI() async {
-         do {
-             try await CoreDataManager.shared.loadInitialDataFromAPI() // Загружаем данные только при первом запуске
-             await MainActor.run {
-                 self.fetchToDos()                                     // Обновляем список задач после загрузки
-             }
-         } catch {
-             print("Ошибка загрузки данных из API: \(error)")
-         }
-     }
-     
-     func changeFilterOption(to option: FilterOption) {
-         filterOption = option
-         fetchToDos() // Перезагружаем задачи с новым фильтром
-
-     }
-     
-     func addToDo() {
-         router.navigateToAddToDo { [weak self] title, description, priority, dueDate in
-             self?.interactor.addToDo(title: title, description: description, priority: priority, dueDate: dueDate)
-             self?.fetchToDos()
-         }
-     }
-     
-     // Этот метод вызывается, чтобы перейти на экран редактирования задачи
-     func navigateToEditTodoView(todo: ToDo) {
-         router.navigateToEditTodoView(todo: todo)
-         self.fetchToDos()
-     }
-     
-     func delete(at offsets: IndexSet) {
-         for index in offsets {
-             interactor.deleteToDo(todos[index])
-         }
-         fetchToDos() // Обновляем список после удаления
-     }
-     
-     func delete(todo: ToDo) {
-         interactor.deleteToDo(todo) // Удаляем элемент через Interactor
-         fetchToDos() // Обновляем список задач
-     }
-     
-     func toggleCompletion(for todo: ToDo) {
-         interactor.toggleCompletion(for: todo)
-         fetchToDos()
-     }
-     
-     // Метод для обновления задачи
-     func updateToDo(_ todo: ToDo) {
-         interactor.updateToDo(todo)
-         fetchToDos()
-     }
- }
- */
-
-//NEW
 enum FilterOption {
     case all
     case completed
@@ -168,17 +71,6 @@ final class ToDoListPresenter: NSObject, ObservableObject {
             print("Failed to fetch todos: \(error)")
         }
     }
-    
-    
-    // Обновленный метод для получения задач с учетом текста поиска
-//    func fetchToDos() {
-//        // Получаем задачи с учетом фильтров и поиска
-//        let fetchedTodos = interactor.fetchToDos(filter: filterOption, searchText: searchText)
-//        // Обновляем @Published свойство для перерисовки интерфейса
-//        DispatchQueue.main.async {
-//            self.todos = fetchedTodos // Убедитесь, что создается новый массив
-//        }
-//    }
     
     // Новый метод для фильтрации по поисковому тексту
     func filterToDos(by searchText: String) {
