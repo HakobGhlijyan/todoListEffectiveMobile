147e70be6f4c0a13f7bf3fb0bbd595140175d898
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-11-25 15:40:34 +0400

    [Feature] - Возможность редактирования существующей задачи. UI Not Refresh change task

---
+++ todolist/Module/View/EditToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  EditToDoView.swift
+//  todolist
+//
+//  Created by Hakob Ghlijyan on 25.11.2024.
+//
+
+import SwiftUI
+
+struct EditToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditToDoView()
+}
